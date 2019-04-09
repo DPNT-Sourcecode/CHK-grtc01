241e06658676bc4bd1662199f1965466067442b2
sbx_user1051 <sbx_user1051@169.254.14.245>
2019-04-09 15:03:03 +0000

    Tue Apr 09 15:03:03 UTC 2019

--- spec/CHK/checkout_spec.rb
+++ spec/CHK/checkout_spec.rb
@@ -3,6 +3,7 @@ require './lib/solutions/CHK/checkout.rb'
 describe Checkout do
 
   it 'App should say return -1 if X is in the basket' do
-    expect(subject.checkout('X')).to eq(-1)
+    expect(subject.checkout('XXXX')).to eq(-1)
   end
 end
+
