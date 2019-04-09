9d934cdbcfdbd519fbb176dee3df08cc5dae8142
sbx_user1051 <sbx_user1051@169.254.14.245>
2019-04-09 15:12:03 +0000

    Tue Apr 09 15:12:03 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,20 +1,26 @@
 # noinspection RubyUnusedLocalVariable
 class Checkout
 
-  attr_reader :A, :B, :C, :D
+  attr_reader :price_A, :price_B, :price_C, :price_D
+
+  A = 50
+  B = 30
+  C = 20
+  D = 15
 
   def checkout(skus)
+    p skus
     update_prices
     sorted_basket = skus
     if !check_valid(sorted_basket)
       return -1
     end
   end
-  def update_prices
-    A = 50
-    B = 30
-    C = 20
-    D = 15
+  def update_prices()
+    price_A = A
+    price_B = B
+    price_C = C
+    price_D = D
   end
   def check_valid(basket)
     p  basket.squeeze
@@ -25,4 +31,5 @@ class Checkout
     basket.length == 0
   end
 end
+
 
