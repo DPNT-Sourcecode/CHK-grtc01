ba9828bcd711930c00baab960a70c26cdc9a02f6
sbx_user1051 <sbx_user1051@169.254.14.245>
2019-04-09 15:09:03 +0000

    Tue Apr 09 15:09:03 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,11 +1,20 @@
 # noinspection RubyUnusedLocalVariable
 class Checkout
 
+  attr_reader :A, :B, :C, :D
+
   def checkout(skus)
+    update_prices
     sorted_basket = skus
     if !check_valid(sorted_basket)
       return -1
     end
+  end
+  def update_prices
+    A = 50
+    B = 30
+    C = 20
+    D = 15
   end
   def check_valid(basket)
     p  basket.squeeze
@@ -16,3 +25,4 @@ class Checkout
     basket.length == 0
   end
 end
+
--- spec/CHK/checkout_spec.rb
+++ spec/CHK/checkout_spec.rb
@@ -3,6 +3,10 @@ require './lib/solutions/CHK/checkout.rb'
 describe Checkout do
 
   it 'App should say return -1 if X is in the basket' do
-    expect(subject.checkout('XXXX')).to eq(-1)
+    expect(subject.checkout('X')).to eq(-1)
+  end
+  it 'App should say return 50 if A is in the basket' do
+    expect(subject.checkout('A')).to eq(50)
   end
 end
+
